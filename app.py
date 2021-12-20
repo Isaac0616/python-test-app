6e5aa590e33614c45a214f651c527ac435c60f27
Po-Ning Tseng <poning@dropbox.com>
2021-12-19 21:31:12 -0800

    Add violations

--- app.py
+++ app.py
@@ -1,8 +1,27 @@
-from flask import Flask
+import json
 
-app = Flask(__name__)
+from json import dumps
+
+import flask
+
+from flask import request
+
+app = flask.Flask(__name__)
 
 
-@app.route("/")
-def hello():
-    return "Hello World!"
+@app.route("/user")
+def user():
+    user_dict = get_user(request.args.get("id"))
+    # ruleid:use-jsonify
+    return json.dumps(user_dict)
+
+
+@app.route("/user2")
+def user2():
+    user_dict = get_user(request.args.get("id"))
+    # ruleid:use-jsonify
+    return dumps(user_dict)
+
+
+def get_user():
+    return {}
