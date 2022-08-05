258c01e3180cee5251aaf235a88db023dcf5ec9b
Po-Ning Tseng <poning@dropbox.com>
2022-08-05 00:16:02 -0700

    remove system

--- app.py
+++ app.py
@@ -1,5 +1,4 @@
 from flask import Flask, request
-import os
 
 app = Flask(__name__)
 
@@ -7,18 +6,3 @@ app = Flask(__name__)
 @app.route("/")
 def hello():
     return "Hello World!"
-
-
-@app.route("/system")
-def system():
-    return os.system("ls")
-
-
-@app.route("/system2")
-def system2():
-    return os.system(request.args["command"])
-
-
-@app.route("/system3")
-def system3():
-    return os.system(request.args["x"])
